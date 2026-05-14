c1df7d4183a0cc76a4fd7bc914bb51e39adc851d
tj-tamannarana <tamanna.rana@tripjack.com>
2026-05-14 15:42:05 +0530

    Implemented Vehicle-Car-ElectricCar hierarchy with method overriding

---
+++ VehicleHierarchy/src/Main.java
@@ -0,0 +1,37 @@
+class Vehicle {
+    void start(){
+        System.out.println("Vehicle is starting");
+    }
+}
+
+class bike extends Vehicle {
+
+
+    void start(){
+        System.out.println("Bike is starting with key");
+    }
+
+}
+
+class electricBike extends Vehicle{
+
+
+    void start() {
+        System.out.println("electricBike is starting with button");
+    }
+}
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        Vehicle v1 = new Vehicle();
+        Vehicle v2 = new bike();
+        Vehicle v3 = new electricBike();
+
+        v1.start();
+        v2.start();
+        v3.start();
+
+    }
+}
